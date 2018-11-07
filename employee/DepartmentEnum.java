98144f4d5a5dfbecc3d4b4a5bae4ab17d44642ad
inguuu <dlsrb8949@gmail.com>
2018-11-07 13:42:23 +0900

    commit em2

---
+++ employee/DepartmentEnum.java
@@ -0,0 +1,25 @@
+package employee;
+
+public enum DepartmentEnum {
+	GENERAL_AFFAIR(1, "총무"),
+	PERSONNEL(2, "인사"),
+	PLANNING(3, "기획"),
+	PRODUCTION(4, "생산"),
+	SALES(5, "영업");
+
+	int code;
+	String message;
+	
+	DepartmentEnum(int code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+}
