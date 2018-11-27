80c9cdae9d76902823a4601969d9c5ee50c08400
inguuu <dlsrb8949@gmail.com>
2018-11-27 19:12:28 +0900

    commit menu4

---
+++ menus/RegisterMenuEnum.java
@@ -0,0 +1,27 @@
+package menus;
+
+
+
+public enum RegisterMenuEnum {
+
+	REGULAR(11, "정규직 등록"),
+	TEMPORARY(12, "임시직 등록"),
+	RETURN(19, "돌아가기");
+	
+	int code;
+	String message;
+	
+	RegisterMenuEnum(int code, String message) {
+		this.code = code;
+		this.message = message;
+		
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+}
